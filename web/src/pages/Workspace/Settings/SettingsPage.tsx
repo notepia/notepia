3ceb0b9d504030e72e4cf3baf10903fdaf6e10cf
ti777777 <ti12272198686@yahoo.com.tw>
2025-09-06 17:44:31 +0800

    fix text in dark mode on the settings page

--- web/src/pages/Workspace/Settings/SettingsPage.tsx
+++ web/src/pages/Workspace/Settings/SettingsPage.tsx
@@ -69,7 +69,7 @@ const Settings = () => {
                                         {t("pages.settings.workspaceName")}
                                     </div>
                                     <div className="flex gap-3 flex-wrap">
-                                        <input className="flex-1 px-3 py-2 border rounded-lg" value={workspaceName} onChange={e => setWorkspaceName(e.target.value)} title="rename workspace" />
+                                        <input className="flex-1 px-3 py-2 border rounded-lg dark:text-black" value={workspaceName} onChange={e => setWorkspaceName(e.target.value)} title="rename workspace" />
                                         <button onClick={handleRenameClick} className=" px-3 py-2 border shadow-sm rounded-lg">{t("actions.rename")}</button>
                                     </div>
                                 </div>
