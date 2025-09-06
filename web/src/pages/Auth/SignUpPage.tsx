222cd4679c0e90927c27f39df2f552dc021a58af
ti777777 <a104221087@gmail.com>
2025-09-06 19:51:44 +0800

    tweak signin and signup page styles (#14)

--- web/src/pages/Auth/SignUpPage.tsx
+++ web/src/pages/Auth/SignUpPage.tsx
@@ -96,9 +96,9 @@ const SignUp: React.FC = () => {
               required
             />
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex flex-col items-center gap-5 justify-between">
             <button
-              className=" whitespace-nowrap bg-amber-600 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline disabled:opacity-50"
+              className=" w-full bg-amber-600 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline disabled:opacity-50"
               type="submit"
               disabled={signUpMutation.isPending}
             >
