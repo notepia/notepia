86b0d28d6703e3aa4ddcd28160311fb53fb70ff4
ti777777 <ti12272198686@yahoo.com.tw>
2025-09-09 21:15:28 +0800

    fix: update search input placeholder to use translation function

--- web/src/pages/Workspace/Notes/NotesPage.tsx
+++ web/src/pages/Workspace/Notes/NotesPage.tsx
@@ -97,7 +97,7 @@ const Notes = () => {
                         <div className="hidden sm:block">
                              <div className="flex items-center gap-3 py-2 px-3 rounded-md dark:border-neutral-600 bg-neutral-200 dark:bg-neutral-900 dark:text-neutral-100">
                                 <Search size={16} />
-                                <input type="text" className=" flex-1 bg-transparent" placeholder="Search" />
+                                <input type="text" className=" flex-1 bg-transparent" placeholder={t("placeholder.search")} />
                             </div>
                         </div>
                         <div className="flex items-center">
@@ -125,7 +125,7 @@ const Notes = () => {
                 <div className="block sm:hidden">
                     <div className="w-full flex items-center gap-3 py-2 px-3 rounded-md border dark:border-neutral-600 bg-neutral-200 dark:bg-neutral-900 dark:text-neutral-100">
                         <Search size={16} />
-                        <input type="text" className=" bg-transparent flex-1" placeholder="Search" />
+                        <input type="text" className=" bg-transparent flex-1" placeholder={t("placeholder.search")} />
                     </div>                
                 </div>
                 <div className="">
