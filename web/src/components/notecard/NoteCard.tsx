707499a7b04b0cc0f1556102917598c06bc91bbb
ti777777 <a104221087@gmail.com>
2025-12-24 19:34:58 +0800

    refactor: simplify NoteCard component structure and ensure consistent rendering (#295)

--- web/src/components/notecard/NoteCard.tsx
+++ web/src/components/notecard/NoteCard.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 import { NoteData } from "@/api/note"
 import NoteTime from "../notetime/NoteTime"
 import Renderer from "@/components/renderer/Renderer"
+import { ExternalLink } from "lucide-react"
 
 interface NoteCardProps {
     note: NoteData
@@ -11,37 +12,31 @@ interface NoteCardProps {
 }
 
 const NoteCard: FC<NoteCardProps> = ({ note, linkTo }) => {
-    const content = (
-        <>
-            <div className="flex justify-between text-gray-500">
-                <div>
-                    <NoteTime time={note.updated_at ?? ""} />
-                </div>
-            </div>
-            <div className="break-all w-full flex flex-col m-auto">
-                {note.title ? (
-                    <div className="text-2xl font-bold text-gray-900 dark:text-gray-100">
-                        {note.title}
-                    </div>
-                ) : (
-                    <Renderer content={note?.content || ''} />
-                )}
-            </div>
-        </>
-    )
-
-    if (!linkTo) {
-        return (
-            <div className="bg-white dark:bg-neutral-800 border sm:shadow-sm dark:border-none rounded-lg overflow-auto flex flex-col gap-2 p-4">
-                {content}
-            </div>
-        )
-    }
 
     return (
-        <Link to={linkTo} className="bg-white dark:bg-neutral-800 border sm:shadow-sm dark:border-none rounded-lg overflow-auto flex flex-col gap-2 p-4">
-            {content}
-        </Link>
+        <div className="bg-white dark:bg-neutral-800 border sm:shadow-sm dark:border-none rounded-lg overflow-auto flex flex-col gap-2 p-4">
+            <>
+                <div className="flex justify-between items-center text-gray-500">
+                    <div>
+                        <NoteTime time={note.updated_at ?? ""} />
+                    </div>
+                    <div>
+                        <Link to={linkTo || ""}>
+                            <ExternalLink size={16} />
+                        </Link>
+                    </div>
+                </div>
+                <div className="break-all w-full flex flex-col m-auto">
+                    {note.title ? (
+                        <div className="text-2xl font-bold text-gray-900 dark:text-gray-100">
+                            {note.title}
+                        </div>
+                    ) : (
+                        <Renderer content={note?.content || ''} />
+                    )}
+                </div>
+            </>
+        </div>
     )
 }
 
