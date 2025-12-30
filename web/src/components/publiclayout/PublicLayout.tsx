e99cb08ae1ae54133a6c0c9a6cb442579a34598f
ti777777 <a104221087@gmail.com>
2025-12-30 20:43:05 +0800

    fix sidebar button padding in public layout (#323)

--- web/src/components/publiclayout/PublicLayout.tsx
+++ web/src/components/publiclayout/PublicLayout.tsx
@@ -15,28 +15,28 @@ const PublicLayout = () => {
     const sidebarContent = (
         <div className="flex flex-col gap-3">
             <div className="pt-4">
-                <div className={twMerge("flex gap-2 items-center ", isCollapse ? "justify-center": "justify-start px-4")}>
+                <div className={twMerge("flex gap-2 items-center ", isCollapse ? "justify-center": "justify-start px-2")}>
                     <img src={logo} className="w-8 h-8" aria-label="logo" />
                     {!isCollapse && <div className='text-xl font-extrabold text-gray-600 dark:text-gray-400 font-mono'>Notepia</div>}
                 </div>
             </div>
             <div className=" flex flex-col gap-1 overflow-y-auto">
-                <Link to="/explore/notes" className="p-2 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
+                <Link to="/explore/notes" className="p-2.5 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
                     <FileText size={20} />
                     {!isCollapse && <>{t("menu.notes")}</>}
                 </Link>
 
-                <Link to="/explore/calendar" className="p-2 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
+                <Link to="/explore/calendar" className="p-2.5 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
                     <Calendar size={20} />
                     {!isCollapse && <>{t("views.calendar")}</>}
                 </Link>
 
-                <Link to="/explore/map" className="p-2 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
+                <Link to="/explore/map" className="p-2.5 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
                     <MapPin size={20} />
                     {!isCollapse && <>{t("views.map")}</>}
                 </Link>
 
-                <Link to="/explore/kanban" className="p-2 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
+                <Link to="/explore/kanban" className="p-2.5 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
                     <KanbanSquare size={20} />
                     {!isCollapse && <>{t("views.kanban") || "Kanban"}</>}
                 </Link>
@@ -44,11 +44,11 @@ const PublicLayout = () => {
                 <div className="my-2 border-t dark:border-neutral-700"></div>
 
                 {
-                    user ? <Link to="/" className="p-2 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
+                    user ? <Link to="/" className="p-2.5 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
                         <Monitor size={20} />
                         {!isCollapse && <>{t("menu.workspace")}</>}
                     </Link>
-                        : <Link to="/signin" className="p-2 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
+                        : <Link to="/signin" className="p-2.5 flex items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg">
                             <LogIn size={20} />
                             {!isCollapse && <>{t("actions.signin")}</>}
                         </Link>
